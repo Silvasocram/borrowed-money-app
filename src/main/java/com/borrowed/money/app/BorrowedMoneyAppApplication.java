ec0a924e1d1274413329d3a0f580069d0f2bd524
Marcos Silva <silvasocram@gmail.com>
2021-10-07 10:41:01 -0300

    rename package

--- src/main/java/com/borrowed/money/app/BorrowedMoneyAppApplication.java
+++ src/main/java/com/borrowed/money/app/BorrowedMoneyAppApplication.java
@@ -1,4 +1,4 @@
-package com.borrowed.money.borrowedmoneyapp;
+package com.borrowed.money.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
