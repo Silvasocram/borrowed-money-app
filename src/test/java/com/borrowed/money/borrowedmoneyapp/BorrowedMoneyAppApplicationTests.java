3d48fb76154df34103257323d1d52a3b62bee2ea
Marcos Silva <silvasocram@gmail.com>
2021-10-07 10:13:37 -0300

    initial commit

---
+++ src/test/java/com/borrowed/money/borrowedmoneyapp/BorrowedMoneyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borrowed.money.borrowedmoneyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowedMoneyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
